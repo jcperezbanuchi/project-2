c9cd829b6ad7534359567f966110510efb9bccff
Juan Perez Banuchi <juanco@Juans-Air.attlocal.net>
2021-05-27 19:53:33 -0400

    index route created

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const PORT = 3000
+const methodOverride = require('method-override')
+
+//middleware
+app.use(express.urlencoded({ extend: true }))
+// app.use(session({
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+
+//Method Override
+app.use(methodOverride('_method'))
+
+
+app.get('/', (req, res) => {
+    res.send('Covid-19 Vaccination Records')
+})
+
+
+app.get('/records', (req, res) => {
+    res.send('RECORDS')
+})
+
+app.listen(PORT, (req, res) => {
+    console.log('up and running')
+})
