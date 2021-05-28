9b39ce1bbf5c47f27146fc7e4347ce469f4034a1
Juan Perez Banuchi <juanco@Juans-Air.attlocal.net>
2021-05-28 12:19:04 -0400

    new route created

--- controllers/patient.js
+++ controllers/patient.js
@@ -7,17 +7,22 @@ const Patient = require('../models/patient.js')
 
 //index route
 router.get('/', (req, res) => {
-    Patient.find({}, (error, allFruits) => {
+    Patient.find({}, (error, allPatients) => {
         if (error) {
             res.send(error)
         } else {
             res.render('index.ejs', {
-                patient: allPatient
+                patient: allPatients
             });
         }
     })
 });
 
+router.get('/new', (req, res) => {
+    res.render('new.ejs')
+})
+
+//
 router.get('/:id', (req, res) => {
     Patient.findById(req.params.id, (error, foundPatient) => {
         res.render('show.ejs', {
